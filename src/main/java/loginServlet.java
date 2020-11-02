2f2a3f55cdf8249cfd44ed1591cf136430f9a400
NazarValko <66695679+NazarValko@users.noreply.github.com>
2020-11-02 15:05:53 +0200

    Add files via upload

---
+++ src/main/java/loginServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "/loginServlet")
+public class loginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter writer = response.getWriter();
+        String htmlRespone = "<html>";
+        htmlRespone += "<h2>Your username is: " + username + "</h2>";
+        htmlRespone += "</html>";
+
+        writer.println(htmlRespone);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/main_jsp.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Acer
+  Date: 02.11.2020
+  Time: 13:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form name="loginForm" method="post" action="loginServlet">
+    Username: <input type="text" name="username"/> <br/>
+    Password: <input type="password" name="password"/> <br/>
+    <input type="submit" value="Login" />
+</form>
+</body>
+</html>
